0351fd487e5e518ebaf273ca6c2701e29cc890f9
Dev KMK <dev.kmkyaw@gmail.com>
2025-10-23 11:10:46 +0630

    Add footer section to footer.php

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+  <!--footer class-->
+      <footer class="bg-dark pt-5">
+      <div class="copyright">&copy; 2025. All Rights Reserved. Developed by <a href="https://www.burmesecoder.com">Burmese Coder</a></div>
+     </footer>
+
+  </body>
+</html>
